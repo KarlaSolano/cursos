5816c39d8e4fc5c83096ca27d41d65dcb0135dd4
KarlaSolano <36199935+KarlaSolano@users.noreply.github.com>
2025-10-02 19:07:51 -0600

    crear migraciones de las tablas y modelos de categories, couses y pots.

--- database/migrations/2025_10_02_233625_create_posts_table.php
+++ database/migrations/2025_10_02_233625_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,14 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('course_id');
+
             $table->string('name');
+            $table->boolean('free')->default(0);
+
+            $table->foreign('course_id')->references('id')->on('courses');
             $table->timestamps();
         });
     }
@@ -27,6 +32,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('posts');
     }
 }
