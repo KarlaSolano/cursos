a361eede223bd8f9bcc7e73c00c3b724683b5d4e
KarlaSolano <36199935+KarlaSolano@users.noreply.github.com>
2025-10-04 23:05:07 -0600

    crear template para la vista de cursos

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,20 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getImageAttribute($value)
+    {
+        return "https://picsum.photos/seed/course-{$this->id}/1280/720";
+    }
+
+    
+    public function getExcerptAttribute()
+    {
+        return substr($this->description, 0, 80) . "...";
+    }
 }
